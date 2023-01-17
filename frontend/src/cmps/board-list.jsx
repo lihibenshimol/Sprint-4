9bf0b7edc4f2dde7de473082b34e38c34f433320
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2023-01-17 22:57:40 +0200

    Make board list and styles

--- frontend/src/cmps/board-list.jsx
+++ frontend/src/cmps/board-list.jsx
@@ -1,3 +1,33 @@
-export function BoardList(){
-    
+import { useEffect, useState } from "react";
+import { boardService } from "../services/board.service.local";
+import { BoardPreview } from "./board-preview";
+
+
+export function BoardList({ addNew = false }) {
+
+    const [boards, setBoards] = useState([])
+
+    useEffect(() => {
+        loadBoards()
+    }, [])
+
+    async function loadBoards() {
+        try {
+            const boards = await boardService.query()
+            setBoards(boards)
+        } catch (err) {
+            console.log('Had error fetching boards; ', err)
+        }
+    }
+
+    return (
+        <section className="board-list">
+            {addNew && 
+                <article className="add-board"><span>Create new board</span></article>
+            }
+            {boards[0] && boards.map(board => (
+                <BoardPreview board={board} key={board._id} />
+            ))}
+        </section>
+    )
 }
--- frontend/src/pages/boards.jsx
+++ frontend/src/pages/boards.jsx
@@ -2,8 +2,12 @@ import { BoardList } from "../cmps/board-list";
 
 export function Boards() {
     return (
-        <section className="boards">
-            <BoardList />
+        <section className="boards main-layout">
+
+            <section className="your-boards">
+                <h2>Your boards</h2>
+                <BoardList addNew={true}/>
+            </section>
         </section>
     )
 }
