115e50a15adc3c7f631fb5d0ec68b902acc6ec4c
Lihi Ben Shimol <lihibs26@gmail.com>
2023-01-17 20:27:19 +0200

    first demo data

--- frontend/src/pages/board-index.jsx
+++ frontend/src/pages/board-index.jsx
@@ -1,16 +1,27 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import { GroupList } from '../cmps/group-list.jsx'
+import { boardService } from '../services/board.service.local.js'
 
 export function BoardIndex() {
-
+    const [boards, setBoards] = useState()
     // const groups = useSelector(storeState => storeState.groupModule.groups)
 
-    // useEffect(() => {
-    //     loadGroups()
-    // }, [])
+    useEffect(() => {
+        loadBoards()
+    }, [])
 
+
+    async function loadBoards() {
+        try {
+            const boards = await boardService.query()
+            setBoards(boards)
+        } catch (err) {
+            console.log('cannot load')
+            throw err
+        }
+    }
     // async function onRemoveGroup(groupId) {
     //     try {
     //         await removeGroup(groupId)
@@ -46,7 +57,11 @@ export function BoardIndex() {
     //     console.log(`TODO Adding msg to group`)
     // }
 
-    return (
-        <GroupList />
+    return (<>
+        <pre>
+            {JSON.stringify(boards, null, 2)}
+        </pre>
+    </>
+        // <GroupList />
     )
 }
--- frontend/src/services/board.service.local.js
+++ frontend/src/services/board.service.local.js
@@ -1,77 +1,150 @@
 
 import { storageService } from './async-storage.service.js'
 import { utilService } from './util.service.js'
-import { userService } from './user.service.js'
+// import { userService } from './user.service.js'
 
-const STORAGE_KEY = 'car'
+const STORAGE_KEY = 'board'
 
-export const carService = {
+_createDemoBoards()
+
+export const boardService = {
     query,
     getById,
     save,
     remove,
-    getEmptyCar,
-    addCarMsg
+    // getEmptyBoard,
+    // addBoardMsg
 }
-window.cs = carService
+window.cs = boardService
 
 
-async function query(filterBy = { txt: '', price: 0 }) {
-    var cars = await storageService.query(STORAGE_KEY)
-    if (filterBy.txt) {
-        const regex = new RegExp(filterBy.txt, 'i')
-        cars = cars.filter(car => regex.test(car.vendor) || regex.test(car.description))
-    }
-    if (filterBy.price) {
-        cars = cars.filter(car => car.price <= filterBy.price)
-    }
-    return cars
-}
-
-function getById(carId) {
-    return storageService.get(STORAGE_KEY, carId)
+async function query() {
+    const boards = await storageService.query(STORAGE_KEY)
+    // if (filterBy.txt) {
+    //     const regex = new RegExp(filterBy.txt, 'i')
+    //     boards = boards.filter(board => regex.test(board.vendor) || regex.test(board.description))
+    // }
+    // if (filterBy.price) {
+    //     boards = boards.filter(board => board.price <= filterBy.price)
+    // }
+    return boards
 }
 
-async function remove(carId) {
+function getById(boardId) {
+    return storageService.get(STORAGE_KEY, boardId)
+}
+
+async function remove(boardId) {
     // throw new Error('Nope')
-    await storageService.remove(STORAGE_KEY, carId)
+    await storageService.remove(STORAGE_KEY, boardId)
 }
 
-async function save(car) {
-    var savedCar
-    if (car._id) {
-        savedCar = await storageService.put(STORAGE_KEY, car)
+async function save(board) {
+    let savedBoard
+    if (board._id) {
+        savedBoard = await storageService.put(STORAGE_KEY, board)
     } else {
         // Later, owner is set by the backend
-        car.owner = userService.getLoggedinUser()
-        savedCar = await storageService.post(STORAGE_KEY, car)
+        // board.owner = userService.getLoggedinUser()
+        savedBoard = await storageService.post(STORAGE_KEY, board)
     }
-    return savedCar
+    return savedBoard
 }
 
-async function addCarMsg(carId, txt) {
-    // Later, this is all done by the backend
-    const car = await getById(carId)
-    if (!car.msgs) car.msgs = []
+// async function addBoardMsg(boardId, txt) {
+//     // Later, this is all done by the backend
+//     const board = await getById(boardId)
+//     if (!board.msgs) board.msgs = []
 
-    const msg = {
-        id: utilService.makeId(),
-        by: userService.getLoggedinUser(),
-        txt
+//     const msg = {
+//         id: utilService.makeId(),
+//         // by: userService.getLoggedinUser(),
+//         txt
+//     }
+//     board.msgs.push(msg)
+//     await storageService.put(STORAGE_KEY, board)
+
+//     return msg
+// }
+
+// function getEmptyBoard() {
+//     return {
+//         vendor: 'Susita-' + (Date.now() % 1000),
+//         price: utilService.getRandomIntInclusive(1000, 9000),
+//     }
+// }
+
+
+function _createDemoBoards() {
+    let boards = utilService.loadFromStorage(STORAGE_KEY)
+    if (!boards || !boards.length) {
+        boards = [{
+            "_id": "b101",
+            "title": "First board",
+            "isStarred": false,
+            // "createdBy": {
+            //     "_id": "u101",
+            //     "fullname": "Abi Abambi",
+            //     "imgUrl": "http://some-img"
+            // },
+            // "style": {},
+            "labels": [
+                {
+                    "id": "l101",
+                    "title": "Done",
+                    "color": "#61bd4f"
+                },
+                {
+                    "id": "l102",
+                    "title": "Progress",
+                    "color": "#61bd33"
+                }
+            ],
+            // "members": [
+            //     {
+            //         "_id": "u101",
+            //         "fullname": "Tal Tarablus",
+            //         "imgUrl": "https://www.google.com"
+            //     }
+            // ],
+            "groups": [
+                {
+                    "id": "g101",
+                    "title": "Group 1",
+                    // "archivedAt": 1589983468418,
+                    "cards": [
+                        {
+                            "id": "c101",
+                            "title": "Replace logo"
+                        },
+                        {
+                            "id": "c102",
+                            "title": "Add Samples"
+                        }
+                    ],
+                    "style": {}
+                },
+                {
+                    "id": "g102",
+                    "title": "Group 2",
+                    // "archivedAt": 1589983468418,
+                    "cards": [
+                        {
+                            "id": "c103",
+                            "title": "Test groups"
+                        },
+                        {
+                            "id": "c104",
+                            "title": "Test Cards"
+                        }
+                    ],
+                    "style": {}
+                },
+            ]
+        }]
+        utilService.saveToStorage(STORAGE_KEY, boards)
     }
-    car.msgs.push(msg)
-    await storageService.put(STORAGE_KEY, car)
-
-    return msg
 }
-
-function getEmptyCar() {
-    return {
-        vendor: 'Susita-' + (Date.now() % 1000),
-        price: utilService.getRandomIntInclusive(1000, 9000),
-    }
-}
-
 
 // TEST DATA
 // storageService.post(STORAGE_KEY, {vendor: 'Subali Rahok 2', price: 980}).then(x => console.log(x))
