8b979d86ba9fbd2c24dfa0b459abb3c78d7168ac
Lihi Ben Shimol <lihibs26@gmail.com>
2023-01-17 22:00:58 +0200

    group index, list, cards

--- frontend/src/cmps/group-list.jsx
+++ frontend/src/cmps/group-list.jsx
@@ -1,5 +1,17 @@
-export function GroupList(){
+import { GroupPreview } from "./group-preview";
+
+
+export function GroupList({ groups }) {
+
+
     return (
-        <section></section>
+        <>
+        <div className="group-list flex">
+
+        {groups.map(group => <section className="group-wrapper flex" key={group.id}>
+                <GroupPreview group={group} />
+        </section>)}
+        </div>
+        </>
     )
 }
