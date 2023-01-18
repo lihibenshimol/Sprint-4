827acf98ae363a05ba99c5240854dac551b4ebc2
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2023-01-18 16:02:05 +0200

    Change create board dropdown styling and connect adding a board functionality

--- frontend/src/cmps/board-add.jsx
+++ frontend/src/cmps/board-add.jsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
-import { boardService } from "../services/board.service.local"
 import { DropDown } from "./dropdown"
 
 export function BoardAdd() {
-
-    const [board, setBoard] = useState(boardService.getEmptyBoard())
+    
     const [isAddingBoard, setAddingBoard] = useState(false)
-    const navigate = useNavigate()
 
     useEffect(() => {
         document.body.addEventListener('click', handleBodyClick)
@@ -20,22 +17,6 @@ export function BoardAdd() {
     function handleBodyClick({target}) {
         if (target.classList.contains('board-add') || target.parentElement.classList.contains('board-add')) return
         setAddingBoard(false)
-    }
-
-    async function onAddBoard() {
-        const title = prompt('Board name?')
-        if (!title) return
-        board.title = title
-        try {
-            const savedBoard = await boardService.save(board)
-            navigate(`/board/${savedBoard._id}`)
-        } catch (err) {
-            console.log('Had issues creating a board; ', err)
-        }
-
-        // setBoard(prevBoard => {
-        //     return {...prevBoard, title}
-        // })
     }
 
     return (
