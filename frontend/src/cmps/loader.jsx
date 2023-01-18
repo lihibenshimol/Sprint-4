ed73f25662339161fc53e1cabefa834c552cb093
AviadMalikan <aviad879@gmail.com>
2023-01-19 01:41:16 +0200

    Add Trello loader - SVG, CSS and cmp

---
+++ frontend/src/cmps/loader.jsx
@@ -0,0 +1,9 @@
+import LoaderSvg from '../assets/img/loader.svg'
+
+export function Loader() {
+
+
+    return <>
+        <img className='trello-loader' src={LoaderSvg} alt="Loading..." />
+    </>
+}
