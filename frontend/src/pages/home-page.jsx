a7fd879c853c6278f4dd779c5c15efccb84d5f70
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2023-01-18 22:42:49 +0200

    Fix homepage layout

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
 
 export function HomePage() {
 
     return (
-        <section className='home-page full main-container'>
+        <section className='home-page full main-layout'>
             <h2>Trello brings all your tasks, teammates, and tools together</h2>
             <h3>Keep everything in the same place—even if your team isn’t.</h3>
             <Link to="/board">Start demo</Link>
