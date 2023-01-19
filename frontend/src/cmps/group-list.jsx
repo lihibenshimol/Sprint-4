0e7158fd8e75ac1f73cdc1a7f664f26c3f8dbff2
Lihi Ben Shimol <lihibs26@gmail.com>
2023-01-19 17:47:08 +0200

    icons

--- frontend/src/cmps/group-list.jsx
+++ frontend/src/cmps/group-list.jsx
@@ -3,9 +3,12 @@ import { updateBoard } from "../store/board.actions";
 import { GroupDetails } from "./group-details";
 import { useSelector } from "react-redux";
 import { useState } from "react";
-import addIcon from '../assets/img/add.svg';
+import { FiPlus } from 'react-icons/fi'
+import { RxCross2 } from 'react-icons/rx'
 
-export function GroupList({onAddGroup, onAddCard, onRemoveGroup}) {
+
+
+export function GroupList({ onAddGroup, onAddCard, onRemoveGroup }) {
     const board = useSelector(storeState => storeState.boardModule.currBoard)
     const [editMode, setEditMode] = useState(false)
     const [groupToEdit, setGroupToEdit] = useState(boardService.getEmptyGroup())
@@ -16,7 +19,7 @@ export function GroupList({onAddGroup, onAddCard, onRemoveGroup}) {
         setEditMode(!editMode)
         onAddGroup(groupToEdit)
     }
-    
+
     function handleChange({ target }) {
         let { value } = target
         setGroupToEdit((prevGroup) => ({ ...prevGroup, title: value }))
@@ -34,21 +37,31 @@ export function GroupList({onAddGroup, onAddCard, onRemoveGroup}) {
                 </section>)}
 
                 <section className="add-group-section">
-                    <div onClick={() => setEditMode(!editMode)} className={"add-group-btn" + (editMode ? ' edit-mode' : '')}>
-                        <span className="group-list-add-icon">
-                            <img src={addIcon} /> </span>
+                    {/* <div onClick={() => setEditMode(!editMode)} className={"add-group-btn" + (editMode ? ' edit-mode' : '')}>
                         <span className="placeholder">
-                            Add another list
+                            <FiPlus />  Add another list
                         </span>
-                    </div>
+                    </div> */}
 
-                    <form onSubmit={addGroup} className={"group-title-form" + (editMode ? ' edit-mode' : '')} >
-                        <input onChange={handleChange}
-                            spellCheck="false"
-                            dir="auto"
-                            value={groupToEdit.title} />
-                        <button className="btn-add-group">Add list</button>
-                    </form>
+                    <span onClick={() => setEditMode(!editMode)} className={"add-item-btn" + (editMode ? ' edit-mode' : '')}> <FiPlus /> Add another list </span>
+
+                    <div className={"add-item" + (editMode ? ' edit-mode' : '')}>
+                        <form onSubmit={addGroup} >
+                            <textarea
+                                type="text"
+                                name="title"
+                                value={groupToEdit.title}
+                                onChange={handleChange}
+                                placeholder="Enter list title..."
+                            >
+                            </textarea>
+
+                            <span className="add-group-btns">
+                                <button className="save-btn">Add list</button>
+                                <button onClick={() => setEditMode(!editMode)} type="button" className="cancel-btn"><RxCross2 /></button>
+                            </span>
+                        </form>
+                    </div>
                 </section>
             </div>
         </>
