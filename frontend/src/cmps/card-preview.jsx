df950e00a075e963a8e2aaec20e5ebba5a4a5b4f
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2023-01-22 17:54:44 +0200

    Add line to draggable

--- frontend/src/cmps/card-preview.jsx
+++ frontend/src/cmps/card-preview.jsx
@@ -7,6 +7,7 @@ import { QuickEditor } from "./quick-editor";
 
 function Container(props) {
     const { children, innerRef, provided, isDragging } = props
+    console.log(isDragging)
     return <div className={isDragging ? 'card-preview dragging' : 'card-preview'} ref={innerRef}
         {...provided.draggableProps}
         {...provided.dragHandleProps}
@@ -38,15 +39,15 @@ export function CardPreview({ card, idx, groupId }) {
                 <Container
                     provided={provided}
                     innerRef={provided.innerRef}
-                    isDragging={snapshot.isDragging}
+                    isDragging={snapshot.isDragging && !snapshot.isDropAnimating}
                 >
                     <section className="card-title flex">
                         <span>{card.title}</span>
                         <button onClick={(ev) => openQuickEditor(ev)} className="quick-edit-btn"> <BsPencil /> </button>
                     </section>
                     <section className="card-preview-details">
-                        {card.checklists && card.checklists.map(checklist => <span className="preview-details-checklist" key={checklist.id}> {doneInCheckList(checklist)}/{checklist.todos.length} <span className="preview-details-checklist-icon"> <IoMdCheckboxOutline/> </span> </span>)}
-                        {card.members && <span className="preview-details-members"> {card.members.map(member => <span key={member._id}> <img className="member-img" src={member.imgUrl} alt="" /></span> )} </span>}
+                        {card.checklists && card.checklists.map(checklist => <span className="preview-details-checklist" key={checklist.id}> {doneInCheckList(checklist)}/{checklist.todos.length} <span className="preview-details-checklist-icon"> <IoMdCheckboxOutline /> </span> </span>)}
+                        {card.members && <span className="preview-details-members"> {card.members.map(member => <span key={member._id}> <img className="member-img" src={member.imgUrl} alt="" /></span>)} </span>}
                     </section>
                     {quickEditor &&
                         <section onClick={e => e.stopPropagation()}>
