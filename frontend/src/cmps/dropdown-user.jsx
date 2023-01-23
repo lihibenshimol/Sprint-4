80a8abc12bd5812f62d3209a1248d3990ea3561d
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2023-01-23 19:31:51 +0200

    Style user dropdown menu

--- frontend/src/cmps/dropdown-user.jsx
+++ frontend/src/cmps/dropdown-user.jsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import { Link, useNavigate } from "react-router-dom"
 import { loadBoards } from "../store/board.actions"
+import { store } from "../store/store"
+import { logout } from "../store/user.actions"
 
 export function DropdownUser({ setDropDown }) {
 
     const navigate = useNavigate()
+    const user = useSelector(storeState => storeState.userModule.user)
 
 
     function handleLinkClick(ev, boardId) {
@@ -14,11 +17,24 @@ export function DropdownUser({ setDropDown }) {
         setDropDown({})
     }
 
+    function onLogin() {
+        navigate('/login')
+    }
+
+    async function onLogout() {
+        try {
+            await logout()
+            navigate('/login')
+        } catch (err) {
+            console.log('Failed to logout, ', err)
+        }
+    }
+
     return (
-        <section onClick={(ev) => ev.stopPropagation()} className="dropdown dropdown-boards">
-
-            <h2>hello</h2>
-
+        <section onClick={(ev) => { ev.stopPropagation() }} className='dropdown dropdown-user'>
+            <label>Account</label>
+            {!user && <div onClick={onLogin}>Login</div>}
+            {user && <div onClick={onLogout}>Log out</div>}
         </section>
     )
 }
