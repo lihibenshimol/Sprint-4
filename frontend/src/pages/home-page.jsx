a53dd13f124e27af0a6939cef0ed1f3fc5906f4d
Lihi Ben Shimol <lihibs26@gmail.com>
2023-01-23 19:30:46 +0200

    home page

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import collage1 from '../assets/img/homepage-1collage.png'
-import whiteWave from '../assets/img/white-wave-bg.svg'
+import asset1 from '../assets/img/asset1.png'
+import asset2 from '../assets/img/asset2.png'
+import asset3 from '../assets/img/asset3.png'
+import { useState } from 'react'
 
 export function HomePage() {
+    const [dynImg, setDynImg] = useState(asset1)
+
+    function handleClick(imgNum) {
+        setDynImg(`asset+${imgNum}`)
+    }
 
     return (
         <section className='home-page '>
@@ -19,8 +27,6 @@ export function HomePage() {
                     <img src={collage1} alt="img" />
                 </section>
             </div>
-            {/* <div className='white-wave'><img src={whiteWave} alt="waveee" /> </div> */}
-
 
             <div className='app-desc'>
                 <section className='desc'>
@@ -31,9 +37,25 @@ export function HomePage() {
                 </section>
             </div>
 
-            <div className=''>
+            <div className='boards-desc'>
+                <div className='tabs'>
+                    <button onClick={() => setDynImg(asset1)} className='tab tab1'>
+                        <h4>Boards</h4>
+                        <p>Trello boards keep tasks organized and work moving forward. In a glance, see everything from “things to do” to “aww yeah, we did it!”</p>
+                    </button>
+                    <button onClick={() => setDynImg(asset2)} className='tab tab2'>
+                        <h4>Lists</h4>
+                        <p>The different stages of a task. Start as simple as To Do, Doing or Done—or build a workflow custom fit to your team’s needs. There’s no wrong way to Trello.</p>
+                    </button>
+                    <button onClick={() => setDynImg(asset3)} className='tab tab3'>
+                        <h4>Cards</h4>
+                        <p>Cards represent tasks and ideas and hold all the information to get the job done. As you make progress, move cards across lists to show their status.</p>
+                    </button>
+                </div>
 
-
+                <div className='dyn-img'>
+                    <img src={dynImg} alt="" />
+                </div>
             </div>
         </section >
     )
