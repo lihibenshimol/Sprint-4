50763076f649e9072833182866dec7f95d2ce56e
Lihi Ben Shimol <lihibs26@gmail.com>
2023-01-24 12:07:50 +0200

    card preview

--- frontend/src/cmps/card-list.jsx
+++ frontend/src/cmps/card-list.jsx
@@ -10,8 +10,6 @@ export function CardList(props) {
         <section {...provided.droppableProps} ref={innerRef} className={isDraggingOver ? 'card-list drag-over' : 'card-list'}>
             {cards.map((card, idx) =>
                 <Link key={card.id} to={`g/${groupId}/c/${card.id}`}>
-                    {card.cover && <div className="card-preview-cover" style={{ backgroundColor: card.cover }}> </div>}
-                    {/* <button onClick={(ev) => openQuickEditor(ev)} className="quick-edit-btn"> <BsPencil /> </button> */}
                     <CardPreview card={card} groupId={groupId} idx={idx} openQuickEditor={openQuickEditor} />
                 </Link>
             )}
