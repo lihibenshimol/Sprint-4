2d72e3225b16dba3fe1b112d021509b7c599a891
AviadMalikan <aviad879@gmail.com>
2023-01-25 20:23:51 +0200

    update the model of activity on menu section

--- frontend/src/cmps/board-menu.jsx
+++ frontend/src/cmps/board-menu.jsx
@@ -1,21 +1,32 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import { RxCross2 } from "react-icons/rx";
+import { RxActivityLog, RxCross2 } from "react-icons/rx";
 import { loadActivities } from "../store/activity.actions";
+import Moment from 'react-moment';
 
 
 export function BoardMenu({ isOpenMenu, setIsOpenMenu, board }) {
-    const [activities, setActivities] = useState()
+    const [activities, setActivities] = useState([])
 
     useEffect(() => {
         onloadActivities()
     }, [])
 
-    function onloadActivities() {
-        loadActivities()
+    async function onloadActivities() {
+        try {
+            const activities = await loadActivities()
+            setActivities(activities)
+        } catch (err) {
+            console.log('Cant load activities')
 
+        }
     }
 
+
+    function logg() {
+        console.log('activities: ', activities)
+
+    }
     return (
         <div className="extras-board-menu flex" >
             <span className="title-container">
@@ -27,13 +38,24 @@ export function BoardMenu({ isOpenMenu, setIsOpenMenu, board }) {
             <div className="extras-content-menu">
                 <label className="label-option bg-select">
                     <span className="icon" style={board.style}></span>
-                    Background Color
+                    Change background
                 </label>
                 <hr />
-                <label className="label-option activities-label">
-                    <span className="icon" style={board.style}></span>
+                <label className="label-option activities-label" onClick={logg}>
+                    <span className="icon"><RxActivityLog /> </span>
                     Activities
                 </label>
+
+                <article className="activity-container">
+                    <div className="user-activity-img"></div>
+                    <div className="activity-desc">
+                        <h4>User name </h4>
+                        <p>Join to group</p>
+                    </div>
+                    <div className="activity-time">
+                        <Moment interval={10000} fromNow></Moment>
+                    </div>
+                </article>
 
             </div>
         </div>
