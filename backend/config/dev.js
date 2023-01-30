c4ecfedd12a592d50757ac26922e64ce120eed5a
AviadMalikan <aviad879@gmail.com>
2023-01-30 16:45:47 +0200

    connect the backend to atlas

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,4 +1,7 @@
 module.exports = {
-  dbURL: 'mongodb://127.0.0.1:27017',
-  dbName : 'tester_db'
+  dbURL: 'mongodb+srv://Aviad:Aviad0503@cluster0.qly98vy.mongodb.net/?retryWrites=true&w=majority',
+  dbName : 'trellofy_db'
+  
+  // dbURL: 'mongodb://127.0.0.1:27017',
+  // dbName : 'tester_db'
 }
--- frontend/src/cmps/homepage-header.jsx
+++ frontend/src/cmps/homepage-header.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { GrClose } from 'react-icons/gr'
 import { useState } from 'react'
+import mainLogo from '../assets/img/main-logo-black.png'
 
 export function HomepageHeader() {
 
@@ -14,7 +15,8 @@ export function HomepageHeader() {
     return (
         <header className="homepage-header full space-between align-center ">
             <nav className='flex align-center'>
-                <Link to="/"><h1>Trellofy</h1></Link>
+                {/* <Link to="/"><h1>Trellofy</h1></Link> */}
+                <Link to="/"><div className='main-img flex'><img src={`${mainLogo}`} alt="" /></div></Link>
                 <span className={menuOpen ? 'main-layout full open' : 'main-layout full'}>
                     <Link to="/login">Log in</Link>
                     <Link className='cta-btn' to="/signup">Get Trellofy for free</Link>
